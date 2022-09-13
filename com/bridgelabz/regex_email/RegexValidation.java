5086815ea8fe7a2a4b1913e8f6d2d9c61ecce4c2
Shubham Raj <shubhamaryan805@gmail.com>
2022-09-13 13:28:03 +0530

    Added To Begin With Mandatory Part And Start With abc

---
+++ com/bridgelabz/regex_email/RegexValidation.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.regex_email;
+
+import java.util.regex.Pattern;
+
+public class RegexValidation {
+
+    static public boolean isValid(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        return Pattern.matches(regex, input);
+
+    }
+
+    public static void main(String[] args) {
+        boolean regexCheck = isValid("^([a-z]{1,10})$", "shubham");
+        System.out.println(regexCheck);
+    }
+}
